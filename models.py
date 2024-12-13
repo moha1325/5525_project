923032c1d205733a635996f5d3e40b68dc639a4b
Anas M <moha1325@umn.edu>
2024-12-13 11:20:38 -0600

    more data

--- models.py
+++ models.py
@@ -10,10 +10,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 # train and test on our feature set generated using librosa
-# data = pd.read_csv('datasets/gtzan_processed/processed_features.csv')
-# X = data.drop(columns=['genre', 'file_name'])
-# y = data['genre']
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+data = pd.read_csv('datasets/gtzan_all_combined/processed_features.csv')
+X = data.drop(columns=['genre', 'file_name'])
+y = data['genre']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # train and test on 30 second feature set that came with gtzan
 # data = pd.read_csv('datasets/gtzan/features_30_sec.csv')
@@ -22,21 +22,21 @@ from sklearn.metrics import accuracy_score
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # train on the gtzan 3 second feature set, test on the gtzan 30 second feature set
-data3 = pd.read_csv('datasets/gtzan/features_3_sec.csv')
-X3 = data3.drop(columns=['label', 'filename'])
-y3 = data3['label']
-id = np.floor(np.arange(9990)/10)
-gss = GroupShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 42)
-for train_idx3, test_idx3 in gss.split(data3, groups=id):
-    X_train = X3.iloc[train_idx3]
-    y_train = y3.iloc[train_idx3]
-test_idx30 = test_idx3[test_idx3 % 10 == 0]/10
-data30 = pd.read_csv('datasets/gtzan/features_30_sec.csv')
-X30 = data30.drop(columns=['label', 'filename'])
-y30 = data30['label']
-for i in test_idx30:
-    X_test = X30.iloc[test_idx30]
-    y_test = y30.iloc[test_idx30]
+# data3 = pd.read_csv('datasets/gtzan/features_3_sec.csv')
+# X3 = data3.drop(columns=['label', 'filename'])
+# y3 = data3['label']
+# id = np.floor(np.arange(9990)/10)
+# gss = GroupShuffleSplit(n_splits = 10, test_size = 0.2, random_state = 42)
+# for train_idx3, test_idx3 in gss.split(data3, groups=id):
+#     X_train = X3.iloc[train_idx3]
+#     y_train = y3.iloc[train_idx3]
+# test_idx30 = test_idx3[test_idx3 % 10 == 0]/10
+# data30 = pd.read_csv('datasets/gtzan/features_30_sec.csv')
+# X30 = data30.drop(columns=['label', 'filename'])
+# y30 = data30['label']
+# for i in test_idx30:
+#     X_test = X30.iloc[test_idx30]
+#     y_test = y30.iloc[test_idx30]
 
 # standardize the features
 scaler = StandardScaler()
@@ -44,11 +44,11 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # all have the same flow 
-# initialize model -> train -> get accuracy
+# initialize model -> train -> test -> get accuracy
 
 # k nearest neighbors
 def knn(X_train, y_train, X_test, y_test):
-    knn = KNeighborsClassifier(n_neighbors=15)
+    knn = KNeighborsClassifier(n_neighbors=8)
     knn.fit(X_train, y_train)
     y_pred = knn.predict(X_test)
     print("accuracy:", accuracy_score(y_test, y_pred))
@@ -56,19 +56,30 @@ def knn(X_train, y_train, X_test, y_test):
 # svm
 def svm(X_train, y_train, X_test, y_test):
     # rbf kernel
-    svm = SVC(kernel='rbf', C=1, random_state=42)
+    svm = SVC(kernel='rbf', C=10, random_state=42)
     svm.fit(X_train, y_train)
     y_pred = svm.predict(X_test)
     print("accuracy:", accuracy_score(y_test, y_pred))
 
 # random forest
 def random_forest(X_train, y_train, X_test, y_test):
-    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    rf = RandomForestClassifier(n_estimators=800, random_state=42)
     rf.fit(X_train, y_train)
     y_pred = rf.predict(X_test)
     print("accuracy:", accuracy_score(y_test, y_pred))
 
 if __name__ == "__main__":
+    # can try and loop through for more testing
+    paths = [
+        'datasets/gtzan_processed_30s/processed_features.csv', 
+        'datasets/gtzan_processed_15s/processed_features.csv', 
+        'datasets/gtzan_processed_10s/processed_features.csv', 
+        'datasets/gtzan_processed_5s/processed_features.csv', 
+        'datasets/gtzan_processed_3s/processed_features.csv', 
+        'datasets/gtzan_all_combined/processed_features.csv'
+    ]
+
+
     print("knn results")
     knn(X_train, y_train, X_test, y_test)
 
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,41 +1,110 @@
 import librosa
 import numpy as np
-import os
-import matplotlib.pyplot as plt
-import time
-import pandas as pd 
+import pandas as pd
 
-start_time = time.time()
+# will use a "seconds" parameter to figure out how many chunks to use 
+# similar to features_3sec from original gtzan, but with more options
+# so for example if seconds is 5, we will split the audio file into 30/5=6 data points 
+# default is 30 seconds where the entire audio file is a single data point
+def extract_features(audio_path, file_name, seconds=30):
+    num_samples = int(30 // seconds)
 
+    all_features = []
 
-def extract_features(audio_path, file_name):
-    y, sr = librosa.load(audio_path, sr=None)
-    # apply the fourier transform
-    D = librosa.stft(y)
+    for i in range(num_samples):
+        offset = seconds * i 
+        duration = seconds 
+        # make sure to include the very sall portion at the end which would've been lost since duration could lead to an upper bound
+        # might not make a difference but just in case
+        # librosa handles exceeding file length gracefully by just stopping 
+        if offset + duration >= 30:
+            duration += 0.00001
 
-    # magnitude, mfccs, and chroma features
-    magnitude = librosa.amplitude_to_db(abs(D), ref=np.max)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+        # load file and do fourier transform
+        y, sr = librosa.load(audio_path, offset = offset, duration = duration)
+        D = librosa.stft(y)
+        
+        # use things like mean, var to get general information about the data and keep it smaller as well
+
+        # trying to reproduce some gtzan features
+        chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+        rms = librosa.feature.rms(y=y)
+        spectral_centroid = librosa.feature.spectral_centroid(y=y, sr=sr)
+        spectral_bandwidth = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+        spectral_rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+        zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y)
+        y_harmonic, y_percussive = librosa.effects.hpss(y)
+        tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
+        tempo = tempo[0]
+        mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
+
+        chroma_stft_mean = np.mean(chroma)
+        chroma_stft_var = np.var(chroma)
+        rms_mean = np.mean(rms)
+        rms_var = np.var(rms)
+        spectral_centroid_mean = np.mean(spectral_centroid)
+        spectral_centroid_var = np.var(spectral_centroid)
+        spectral_bandwidth_mean = np.mean(spectral_bandwidth)
+        spectral_bandwidth_var = np.var(spectral_bandwidth)
+        spectral_rolloff_mean = np.mean(spectral_rolloff)
+        spectral_rolloff_var = np.mean(spectral_rolloff)
+        zero_crossing_rate_mean = np.mean(zero_crossing_rate)
+        zero_crossing_rate_var = np.var(zero_crossing_rate)
+        harmony_mean = np.mean(y_harmonic)
+        harmony_var = np.var(y_harmonic)
+        perceptr_mean = np.mean(y_percussive)
+        perceptr_var = np.var(y_percussive)
+
+        # features which are not in gtzan
+        # we have tonnetz, magnitude, spectral contrast, spectral flatness
+        magnitude = librosa.amplitude_to_db(abs(D), ref=np.max)
+        tonnetz = librosa.feature.tonnetz(y=librosa.effects.harmonic(y), sr=sr)
+        spectral_contrast = librosa.feature.spectral_contrast(S=np.abs(D), sr=sr)
+        spectral_flatness = librosa.feature.spectral_flatness(y=y)
+        
+        magnitude_mean = np.mean(magnitude)
+        magnitude_var = np.var(magnitude)
+        tonnetz_mean = np.mean(tonnetz)
+        tonnetz_var = np.var(tonnetz)
+        spectral_contrast_mean = np.mean(spectral_contrast, axis=1)
+        spectral_contrast_var = np.var(spectral_contrast, axis=1)
+        spectral_flatness_mean = np.mean(spectral_flatness)
+        spectral_flatness_var = np.var(spectral_flatness)
+        
+        feature = {
+            "file_name": file_name,
+            "magnitude_mean": magnitude_mean,
+            "magnitude_var": magnitude_var,
+            "chroma_stft_mean": chroma_stft_mean,
+            "chroma_stft_var": chroma_stft_var,
+            "rms_mean": rms_mean,
+            "rms_var": rms_var,
+            "spectral_centroid_mean": spectral_centroid_mean,
+            "spectral_centroid_var": spectral_centroid_var,
+            "spectral_bandwidth_mean": spectral_bandwidth_mean,
+            "spectral_bandwidth_var": spectral_bandwidth_var,
+            "rolloff_mean": spectral_rolloff_mean,
+            "rolloff_var": spectral_rolloff_var,
+            "zero_crossing_rate_mean": zero_crossing_rate_mean,
+            "zero_crossing_rate_var": zero_crossing_rate_var,
+            "harmony_mean": harmony_mean,
+            "harmony_var": harmony_var,
+            "perceptr_mean": perceptr_mean,
+            "perceptr_var": perceptr_var,
+            "tempo": tempo,
+            "tonnetz_mean": tonnetz_mean,
+            "tonnetz_var": tonnetz_var,
+            "spectral_flatness_mean": spectral_flatness_mean,
+            "spectral_flatness_std": spectral_flatness_var,
+            **{f"mfcc{i+1}_mean": np.mean(mfccs[i]) for i in range(mfccs.shape[0])},
+            **{f"mfcc{i+1}_var": np.var(mfccs[i]) for i in range(mfccs.shape[0])},
+            **{f"spectral_contrast_{i+1}_mean": spectral_contrast_mean[i] for i in range(len(spectral_contrast_mean))},
+            **{f"spectral_contrast_{i+1}_std": spectral_contrast_var[i] for i in range(len(spectral_contrast_var))}
+        }
+
+        all_features.append(feature)
+    return all_features
     
-    # use things like mean, std to keep amount of data lower
-    magnitude_mean = np.mean(magnitude)
-    magnitude_std = np.std(magnitude)
-    mfccs_mean = np.mean(mfccs, axis=1)
-    mfccs_std = np.std(mfccs, axis=1)
-    chroma_mean = np.mean(chroma, axis=1)
-    chroma_std = np.std(chroma, axis=1)
-    
-    return {
-        "file_name": file_name,
-        "magnitude_mean": magnitude_mean,
-        "magnitude_std": magnitude_std,
-        **{f"mfcc_{i+1}_mean": mfccs_mean[i] for i in range(len(mfccs_mean))},
-        **{f"mfcc_{i+1}_std": mfccs_std[i] for i in range(len(mfccs_std))},
-        **{f"chroma_{i+1}_mean": chroma_mean[i] for i in range(len(chroma_mean))},
-        **{f"chroma_{i+1}_std": chroma_std[i] for i in range(len(chroma_std))}
-    }
-
 
 audio_path = 'datasets/gtzan/genres_original/blues/blues.00000.wav'  
 genres = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal', 'pop', 'reggae', 'rock']
@@ -44,26 +113,32 @@ base_path = 'datasets/gtzan/genres_original'
 features_list = []
 count = 0
 
+# the amount of seconds each sample will be 
+# will be doing 30 seconds(full file) along with 15, 10, 5, 3, 2
+splits_seconds = [30, 15, 10, 5, 3]
 
 # we skip jazz 54 since that is known to be corrupted
+for s in splits_seconds:
+    for g in genres:
+        for i in range(100):
+            num = f"000{str(i)}"
+            if i < 10:
+                num = f"0000{str(i)}"
+            
+            if i == 54 and g == "jazz":
+                continue
+            
+            file_name = f"/{g}/{g}.{num}.wav"
+            full_path = base_path + file_name
+            print(full_path)
+            feature = extract_features(full_path, file_name, s)
+            for f in feature:
+                f['genre'] = g 
+                features_list.append(f)
 
-for g in genres:
-    for i in range(100):
-        num = f"000{str(i)}"
-        if i < 10:
-            num = f"0000{str(i)}"
-        
-        if i == 54 and g == "jazz":
-            continue
-        
-        file_name = f"/{g}/{g}.{num}.wav"
-        full_path = base_path + file_name
-        print(full_path)
-        feature = extract_features(full_path, file_name)
-        feature['genre'] = g 
-        features_list.append(feature)
-
-print(len(features_list))
-df = pd.DataFrame(features_list)
-df.to_csv('datasets/gtzan_processed/processed_features.csv', index=False)
-
+    print(len(features_list))
+    df = pd.DataFrame(features_list)
+    df.to_csv(f'datasets/gtzan_processed_{s}s/processed_features.csv', index=False)
+    # reset features list for next set of samples
+    # if doing all combined then comment out the reset and move the saving to csv lines above this outside the loop
+    features_list = []
